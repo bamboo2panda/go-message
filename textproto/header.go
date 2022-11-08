0ad4703c2aea9b49330d6e330d91cf6e3408dcbc
Pavel Vasilyev <bamboo2panda@gmail.com>
2022-11-08 16:23:48 +0300

    Update header.go

--- textproto/header.go
+++ textproto/header.go
@@ -541,7 +541,8 @@ func ReadHeader(r *bufio.Reader) (Header, error) {
 		// appear in the wild, violating specs, so we remove them if present.
 		i := bytes.IndexByte(kv, ':')
 		if i < 0 {
-			return newHeader(fs), fmt.Errorf("message: malformed MIME header line: %v", string(kv))
+			continue
+			// return newHeader(fs), fmt.Errorf("message: malformed MIME header line: %v", string(kv))
 		}
 
 		keyBytes := trim(kv[:i])
